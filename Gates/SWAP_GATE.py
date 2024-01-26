c37976f38bdc1d57abe871c68426b112b40aa8a0
justinnp <justinmpowell25@gmail.com>
2024-01-25 21:45:03 -0800

    added swap gate in qiskit

---
+++ Gates/SWAP_GATE.py
@@ -0,0 +1,10 @@
+from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
+from numpy import pi
+
+qreg_q = QuantumRegister(2, 'q')
+creg_c = ClassicalRegister(4, 'c')
+circuit = QuantumCircuit(qreg_q, creg_c)
+
+circuit.cx(qreg_q[0], qreg_q[1])
+circuit.cx(qreg_q[1], qreg_q[0])
+circuit.cx(qreg_q[0], qreg_q[1])
