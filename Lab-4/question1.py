f072466bab79a33e8784648d88f06c7b9e2fc423
justinnp <justinmpowell25@gmail.com>
2024-04-21 15:39:41 -0700

    q1 and q2 complete, tg

--- Lab-4/question1.py
+++ Lab-4/question1.py
@@ -40,22 +40,24 @@ def decoder1():
     ############################################################################
 
     qc = QuantumCircuit(5,2)
-    # Error Detection
-    qc.cx(0, 3)
-    qc.cx(1, 3)
-    qc.cx(1, 4)
-    qc.cx(2, 4)
-    # Classical measurement on ancilla
+    # Error Detection - basic circuit layout
+    qc.cx(0, 3) # Z1
+    qc.cx(1, 3) # Z2
+    qc.cx(1, 4) # Z2
+    qc.cx(2, 4) # Z3
+    # Classical measurement on ancilla - measured in little endian format
+    # qubit 3 holds s1 = Z1 x Z2
     qc.measure(3, 1)
+    # qubit 4 holds s2 = Z2 x Z3 
     qc.measure(4, 0)
-    # Correction (flip if ancilla classical 0 is 1 and 1 is 0)
-    # if b'01' then apply X to third qubit
+    # Correction
+    # if binary '01' [MSB = 0(+1) LSB = 1(-1)] then apply X to qubit 2
     qc.x(2).c_if(qc.cregs[0], 1)
     # if b'10' then apply X to qubit 0
     qc.x(0).c_if(qc.cregs[0], 2)
-    # if b'11' then apply X to second qubit
+    # if b'11' then apply X to qubit 1
     qc.x(1).c_if(qc.cregs[0], 3)
-    # Decode
+    # Decode - reversal of encoder
     qc.cx(0, 2)
     qc.cx(0, 1)
     ############################################################################
@@ -77,8 +79,34 @@ def decoder2():
     # Student code begin
     ############################################################################
 
-    return qc
-    
+    qc = QuantumCircuit(5,2)
+    # Error Detection - basic circuit layout
+    qc.cx(0, 3) # Z1
+    qc.cx(1, 3) # Z2
+    qc.cx(1, 4) # Z2
+    qc.cx(2, 4) # Z3
+    # Classical measurement on ancilla - measured in little endian format
+    # qubit 3 holds s1 = Z1 x Z2
+    qc.measure(3, 1)
+    # qubit 4 holds s2 = Z2 x Z3 
+    qc.measure(4, 0)
+
+    # Correction - invert 1-bit correction logic to implement 2-bit QEC knowing exactly 2 errors exist
+    # under 1-bit QEC, if binary '01' [MSB = 0(+1) LSB = 1(-1)] then apply X to qubit 2
+    # under 2-bit QEC, qubit 2 is correct, apply X to qubit 0 and 1
+    qc.x(0).c_if(qc.cregs[0], 1)
+    qc.x(1).c_if(qc.cregs[0], 1)
+    # 1-bit QEC, if b'10' then apply X to qubit 0
+    # 2-bit QEC, X on qubit 1 and 2
+    qc.x(1).c_if(qc.cregs[0], 2)
+    qc.x(2).c_if(qc.cregs[0], 2)
+    # 1-bit QEC, if b'11' then apply X to qubit 1
+    # 2-bit QEC, X on qubit 0 and 2
+    qc.x(0).c_if(qc.cregs[0], 3)
+    qc.x(2).c_if(qc.cregs[0], 3)
+    # Decode
+    qc.cx(0, 2)
+    qc.cx(0, 1)
     ############################################################################
     # Student code end
     ############################################################################
--- Lab-4/question2.py
+++ Lab-4/question2.py
@@ -16,10 +16,10 @@ def encoder():
     # Student code begin
     ############################################################################
 
-    raise NotImplementedError(
-            "`encoder` function in "
-            + "`question2.py` needs to be implemented"
-        )
+    qc.cx(0, 1)
+    qc.cx(0, 2)
+    for i in range(3):
+        qc.h(i)
     
     ############################################################################
     # Student code end
@@ -40,11 +40,31 @@ def decoder1():
     ############################################################################
     # Student code begin
     ############################################################################
-
-    raise NotImplementedError(
-            "`decoder1` function in "
-            + "`question2.py` needs to be implemented"
-        )
+    qc = QuantumCircuit(5, 2)
+    # Detection - basic circuit layout
+    for i in range(3):
+        qc.h(i)
+    qc.cx(0, 3) # X1
+    qc.cx(1, 3) # X2
+    qc.cx(1, 4) # X2
+    qc.cx(2, 4) # X3
+    for i in range(3):
+        qc.h(i)
+    # Measurement - little endian format
+    qc.measure(3, 1) # holds s1 = X1 x X2
+    qc.measure(4, 0) # holds s2 = X2 x X3
+    # Correction
+    # if binary '10' then error location is at qubit 0, apply Z gate
+    qc.z(0).c_if(qc.cregs[0], 2)
+    # if binary '11' then error location is at qubit 1
+    qc.z(1).c_if(qc.cregs[0], 3)
+    # if binary '01' then error location is at qubit 2
+    qc.z(2).c_if(qc.cregs[0], 1)
+    # Decode
+    for i in range(3):
+        qc.h(i)
+    qc.cx(0, 2)
+    qc.cx(0, 1)
     
     ############################################################################
     # Student code end
@@ -66,10 +86,35 @@ def decoder2():
     # Student code begin
     ############################################################################
 
-    raise NotImplementedError(
-            "`decoder2` function in "
-            + "`question2.py` needs to be implemented"
-        )
+    qc = QuantumCircuit(5, 2)
+    # Detection - basic circuit layout
+    for i in range(3):
+        qc.h(i)
+    qc.cx(0, 3) # X1
+    qc.cx(1, 3) # X2
+    qc.cx(1, 4) # X2
+    qc.cx(2, 4) # X3
+    for i in range(3):
+        qc.h(i)
+    # Measurement - little endian format
+    qc.measure(3, 1) # holds s1 = X1 x X2
+    qc.measure(4, 0) # holds s2 = X2 x X3
+
+    # Correction - inverse of 1-bit QEC
+    # if binary '10' then error location at qubit 1 and 2, apply Z
+    qc.z(1).c_if(qc.cregs[0], 2)
+    qc.z(2).c_if(qc.cregs[0], 2)
+    # if binary '11' then error location at qubit 0 and 2
+    qc.z(0).c_if(qc.cregs[0], 3)
+    qc.z(2).c_if(qc.cregs[0], 3)
+    # if binary '01' then error location at qubit 0 and 1
+    qc.z(0).c_if(qc.cregs[0], 1)
+    qc.z(1).c_if(qc.cregs[0], 1)
+     # Decode
+    for i in range(3):
+        qc.h(i)
+    qc.cx(0, 2)
+    qc.cx(0, 1)
     
     ############################################################################
     # Student code end
